796e9b06eba4925e0401b920577e16c245147d8b
David Yell <dyell@ukwebmedia.com>
2016-09-06 11:25:01 +0100

    Refactored the js to remove the repeated code

--- webroot/js/common.js
+++ webroot/js/common.js
@@ -14,4 +14,53 @@ $(function () {
 //  Sticky table headers
     $('#unittable').stickyTableHeaders();
 
+//  Show and hide buttons for slides
+    var hide = '<span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span> Hide';
+    var show = '<span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span> Show';
+
+//  Show and or hide a form well element
+    function toggleWell(well, form, button) {
+        button.click(function (e) {
+            e.preventDefault();
+            form.slideToggle({
+                start: function (animation) {
+                    if ($(this).is(':visible')) {
+                        button.html(hide);
+                    } else {
+                        button.html(show);
+                    }
+                },
+                complete: function () {
+                    if ($(this).is(':visible')) {
+                        button.html(hide);
+                    } else {
+                        button.html(show);
+                    }
+                }
+            });
+        });
+    }
+
+//  Show/Hide for the search form
+    if (window.location.search.indexOf('search=1') !== -1) {
+        $('div.search.well a.show-hide').html(hide);
+    } else {
+        $('div.search.well div.search-form').hide();
+    }
+
+    var searchWell = $('div.search.well');
+    var searchForm = $('div.search.well div.search-form');
+    var searchButton = $('div.search.well a.show-hide');
+
+    toggleWell(searchWell, searchForm, searchButton);
+
+//  Show/Hide for the add form
+    $('div.add-units.well div.add-form').hide();
+
+    var addWell = $('div.add-units.well');
+    var addForm = $('div.add-units.well div.add-form');
+    var addButton = $('div.add-units.well a.show-hide');
+
+    toggleWell(addWell, addForm, addButton);
+
 });
